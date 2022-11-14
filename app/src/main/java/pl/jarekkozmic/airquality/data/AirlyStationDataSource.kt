95a4676720dc0eaf780b12a401e303fb72d5638d
Mateusz Zając <mateusz.zajac@vmlyr.com>
2022-11-14 15:06:22 +0100

    Airly DTOs added

--- app/src/main/java/pl/jarekkozmic/airquality/data/AirlyStationDataSource.kt
+++ app/src/main/java/pl/jarekkozmic/airquality/data/AirlyStationDataSource.kt
@@ -1,12 +1,13 @@
 package pl.jarekkozmic.airquality.data
 
+import com.google.gson.annotations.SerializedName
 import pl.jarekkozmic.airquality.entity.AQStation
 import pl.jarekkozmic.airquality.logic.RemoteStationsRepository
 import retrofit2.http.GET
 
 class AirlyStationDataSource : RemoteStationsRepository {
     override suspend fun getAll(): List<AQStation> {
-
+        return emptyList()
     }
 
     interface AirlyService {
@@ -14,5 +15,46 @@ class AirlyStationDataSource : RemoteStationsRepository {
         suspend fun getInstallations(): List<Installation>
     }
 
-    data class Installation()
+
+    data class Installation (
+
+        @SerializedName("id") var id: Int? = null,
+        @SerializedName("location") var location: Location? = Location(),
+        @SerializedName("locationId") var locationId: Int? = null,
+        @SerializedName("address") var address: Address? = Address(),
+        @SerializedName("elevation") var elevation: Double? = null,
+        @SerializedName("airly") var airly: Boolean? = null,
+        @SerializedName("sponsor") var sponsor: Sponsor? = Sponsor()
+
+    )
+
+    data class Location(
+
+        @SerializedName("latitude") var latitude: Double? = null,
+        @SerializedName("longitude") var longitude: Double? = null
+
+    )
+
+    data class Address(
+
+        @SerializedName("country") var country: String? = null,
+        @SerializedName("city") var city: String? = null,
+        @SerializedName("street") var street: String? = null,
+        @SerializedName("number") var number: String? = null,
+        @SerializedName("displayAddress1") var displayAddress1: String? = null,
+        @SerializedName("displayAddress2") var displayAddress2: String? = null
+
+    )
+
+
+    data class Sponsor(
+
+        @SerializedName("id") var id: Int? = null,
+        @SerializedName("name") var name: String? = null,
+        @SerializedName("description") var description: String? = null,
+        @SerializedName("logo") var logo: String? = null,
+        @SerializedName("link") var link: String? = null,
+        @SerializedName("displayName") var displayName: String? = null
+
+    )
 }
