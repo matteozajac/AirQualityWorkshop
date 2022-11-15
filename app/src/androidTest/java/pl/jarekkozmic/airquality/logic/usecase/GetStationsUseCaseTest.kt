50f825cc4876744049b89982f9f37815c8e33444
Mateusz Zając <mateusz.zajac@vmlyr.com>
2022-11-15 09:33:21 +0100

    Added GetStationsUseCase empty test file

---
+++ app/src/androidTest/java/pl/jarekkozmic/airquality/logic/usecase/GetStationsUseCaseTest.kt
@@ -0,0 +1,13 @@
+package pl.jarekkozmic.airquality.logic.usecase
+
+import org.junit.Test
+
+
+class GetStationsUseCaseTest {
+
+    @Test
+    fun init_DoesNotMakeAnyRemoteOrLocalCalls() {
+        // TODO
+    }
+
+}
